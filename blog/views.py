bb7af88f2fa0b2f3f9e89ce5cea0fd9aec94ad3e
Jacques-Drumond <jacques.junio@hotmail.com>
2023-05-26 15:41:55 -0300

    fourth

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def starting_page(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    return HttpResponse("Pagina Posts") 
+    return render(request, "blog/all_posts.html")
 
 def post_detail(request):
     pass
