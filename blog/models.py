34816c9ead06d07b3a996f276b015a0fcc8c0da2
Jacques Drumond <jacques.drumond@matrix.local>
2023-09-01 14:07:27 -0300

    adding author formation

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,8 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-
+    formation = models.CharField(max_length=100, null=True)
+    
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
